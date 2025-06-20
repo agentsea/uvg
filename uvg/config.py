93e04669887c2abb77434e32e186f9f457d4e86b
nph4rd <arturomf94@gmail.com>
2025-06-20 00:23:01 +0000

    update defaults

--- uvg/config.py
+++ uvg/config.py
@@ -7,19 +7,19 @@ class Config:
     model_id: str
     collate_fn: Callable[[list[dict]], list[dict]] | None = None
     no_apply_chat_template: bool = False
-    max_completion_len: int = 200
+    max_completion_len: int = 2048
     num_generations: int = 8
     num_epochs: int = 1
     learning_rate: float = 5e-6
-    weight_decay: float = 0.1
+    weight_decay: float = 0.01
     warmup_ratio: float = 0.1
-    grad_norm: float = 0.1
+    grad_norm: float = 0.001
     epsilon: float = 0.2
     epsilon_high: float = 0.2
-    beta: float = 0.04
-    temperature: float = 0.9
+    beta: float = 0.001
+    temperature: float = 1.0
     top_p: float = 1.0
-    top_k: int = 50
+    top_k: int | None = None
     min_p: float | None = None
     repetition_penalty: float = 1.0
     bf16: bool = False
